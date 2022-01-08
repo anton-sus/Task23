de998d70951ea3a23ba14982a1ae9d40ff606fea
Anton S <geodesist.sushkov@gmail.com>
2022-01-09 00:50:28 +0300

    Добавьте файлы проекта.

---
+++ Task23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Fact(n);
+            Console.ReadKey();
+        }
+
+        static void Fact(int n)
+        {
+            int f = 1;
+            for (int i = 1; i < n+1; i++)
+            {
+                f *= i;
+                Thread.Sleep(10);
+                //Console.WriteLine(f);
+            }
+            Console.WriteLine($"{n}!={f}");
+        }
+
+        static async void FactAsync(int n)
+        {
+        await Task.Run(()=>Fact(n));
+        }
+    }
+}
